6589f92920b3d7b9a31bff3141a12f42d166043a
mazennmo <elbeltagy.m@oundleschool.org.uk>
2024-10-15 09:19:37 +0100

    qu1

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php
+echo "OOOOOOOOOO<br>";
+echo "O &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; O<br>";
+echo "O &nbsp; &nbsp; # &nbsp; &nbsp; &nbsp; &nbsp; # &nbsp; &nbsp; O<br>";
+echo "O &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; # &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;O<br>";
+echo "O &nbsp; &nbsp; # &nbsp; &nbsp; &nbsp; &nbsp; # &nbsp; &nbsp; O<br>";
+echo "O &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; O<br>";
+echo "OOOOOOOOOO<br>";
+?>
+
+</body>
+</html>
+ 
